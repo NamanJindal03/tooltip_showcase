2754bea7c3e87b2f550a69388657e55deab4c6f1
Naman Jindal <namanjindal113@gmail.com>
2020-08-10 00:21:22 +0530

    Updates

---
+++ precache-manifest.b4ccdf87900f6ed3feec3928c4020c0c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8eee62cc59b3effd0ac1e51a441725b",
+    "url": "/NamanJindal03/tooltip_showcase/index.html"
+  },
+  {
+    "revision": "a2133a70518fce408fb5",
+    "url": "/NamanJindal03/tooltip_showcase/static/css/main.529e6bff.chunk.css"
+  },
+  {
+    "revision": "cec888c70bb242415023",
+    "url": "/NamanJindal03/tooltip_showcase/static/js/2.9113cbe7.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/NamanJindal03/tooltip_showcase/static/js/2.9113cbe7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a2133a70518fce408fb5",
+    "url": "/NamanJindal03/tooltip_showcase/static/js/main.fd19dce7.chunk.js"
+  },
+  {
+    "revision": "75fd84c105a7db9a78dd",
+    "url": "/NamanJindal03/tooltip_showcase/static/js/runtime-main.b259e098.js"
+  }
+]);
